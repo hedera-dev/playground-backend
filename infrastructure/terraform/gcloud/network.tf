9ea65f7c1e65fc31b239671562dd9eec2bb29c0d
Patricia Ramos <patri@kabila.app>
2024-11-06 10:40:56 +0100

    TAS-16740: Secure network, only allow eggress to hedera testnet

--- infrastructure/terraform/gcloud/network.tf
+++ infrastructure/terraform/gcloud/network.tf
@@ -27,7 +27,7 @@ resource "google_compute_firewall" "allow_http" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080"]
+    ports    = ["80", "443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
@@ -52,3 +52,55 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags             = null
   target_service_accounts = null
 }
+
+
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal-${local.deployment["environment_id"]}"
+  network = google_compute_network.network.id
+
+  direction = "EGRESS"
+  priority  = 1000
+
+  source_ranges = ["10.0.0.0/8"]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+
+resource "google_compute_firewall" "allow_hedera_testnet" {
+  name    = "allow-hedera-testnet-${local.deployment["environment_id"]}"
+  network = google_compute_network.network.id
+
+  direction = "EGRESS"
+  priority  = 1000
+
+  destination_ranges = [
+    "34.94.106.61/32", "50.18.132.211/32", "35.237.119.55/32", 
+    "3.212.6.13/32", "35.245.27.193/32", "52.20.18.86/32", 
+    "34.83.112.116/32", "54.70.192.33/32", "34.94.160.4/32", 
+    "54.176.199.109/32", "34.106.102.218/32", "35.155.49.147/32",
+    "34.133.197.230/32", "52.14.252.207/32"
+  ]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+
+resource "google_compute_firewall" "deny_egress_all" {
+  name    = "deny-egress-all-${local.deployment["environment_id"]}"
+  network = google_compute_network.network.id
+
+  direction = "EGRESS"
+  priority  = 5000
+
+  destination_ranges = ["0.0.0.0/0"]
+  deny {
+    protocol = "all"
+  }
+}
+
