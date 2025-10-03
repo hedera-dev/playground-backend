e9cdb49f1c7a5d8b73d480b8097940ccdbc6f65a
Patricia Ramos <patri@kabila.app>
2025-10-03 12:55:01 +0200

    feat(ai-assistant): add output for egress nat ip
    Signed-off-by: Patricia Ramos <patri@kabila.app>

--- infrastructure/terraform/gcloud/app/ai-assistant/outputs.tf
+++ infrastructure/terraform/gcloud/app/ai-assistant/outputs.tf
@@ -3,3 +3,7 @@ output "static_lb_ip" {
   value       = data.google_compute_global_address.lb_static_ip.address
 }
 
+output "egress_nat_ip" {
+  value       = google_compute_address.egress_nat_ip.address
+  description = "Public IP for the Egress NAT"
+}
